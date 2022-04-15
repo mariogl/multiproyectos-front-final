ab19f921f0764a3a6a4e6d03e356b461feab26d0
Mario González <mario@mariogl.com>
2022-04-15 23:06:33 +0200

    Created store and provider

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,17 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
+import store from "./redux/store";
+import { Provider } from "react-redux";
 
 const container = document.getElementById("root");
 const root = createRoot(container as HTMLElement);
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
