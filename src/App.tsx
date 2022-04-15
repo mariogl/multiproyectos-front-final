8781b994d145f6a6cdf559b6f62d0a88c0f6159c
Mario González <mario@mariogl.com>
2022-04-15 20:07:07 +0200

    Layout and navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+import Navigation from "./components/Navigation/Navigation";
+
 function App() {
   return (
     <>
       <header>
         <h1>Challenges</h1>
+        <Navigation />
       </header>
       <main>
         <h2>Listado</h2>
