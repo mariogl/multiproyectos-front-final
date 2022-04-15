26be37e2c07c8eda00180ce05e3c2e0ca4dc9e9e
Mario González <mario@mariogl.com>
2022-04-15 21:14:03 +0200

    Tested load challenges action creator

---
+++ src/redux/actions/challengesActionCreators.ts
@@ -0,0 +1,12 @@
+import LoadChallengesAction from "../../types/actions";
+import Challenge from "../../types/challenges";
+import actionTypes from "./actionTypes";
+
+const loadChallengesAction = (
+  challenges: Challenge[]
+): LoadChallengesAction => ({
+  type: actionTypes.load,
+  challenges,
+});
+
+export default loadChallengesAction;
